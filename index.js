724e4db261e0c3a93dc0e5fdb222c23090834a2f
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-07-03 23:48:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+// const cards = ["Guadalupe", "Ollie", "Aki"];
+
+// function writeCards(cards) {
+//     for (let i = 0; i <cards.length; i++) {
+//         console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+//     }
+//     return cards;
+// }
+// writeCards(cards);
+
+const name = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(name, surprise){
+  let invitations = [];
+  for (let i=0; i < name.length; i++){
+    invitations.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`);
+  }
+  return invitations;
+}
+console.log(writeCards());
+
+function countDown(){
+  let num = 10;
+  while(num >= 0){
+    console.log(num);
+    num = num -1;
+  }
+}
+countDown()
